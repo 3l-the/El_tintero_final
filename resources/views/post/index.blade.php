c7cd6c074a5df402adceacbca3cdda78ad354ad3
Eduardo Mulhia <77212608+3l-the@users.noreply.github.com>
2025-03-30 20:28:05 -0600

    Detalles bootstrap

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,29 +9,33 @@
     @include('common.header')
 </header>
 <body>
+    <div class="container mt-3">
+        <h3>Ultimas publicaciones</h3>
+    </div>
     <div class="container flex-wrap mt-3">
-    @forelse($posts as $post)
-        <div class="card mb-2 rounded">
-            <div>
-                <a class="subject" href="{!! url('/post/detail', [$post['post_id']]) !!}">
-                    {{ $post['post_subject'] }}
-                </a> 
+        @forelse($posts as $post)
+            <div class="card mb-2 rounded">
+                <div>
+                    <a class="subject" href="{!! url('/post/detail', [$post['post_id']]) !!}">
+                        {{ $post['post_subject'] }}
+                    </a> 
+                </div>
+                <div>
+                    <a class="user" href="{!! url('/post/user', [$post['user_id']]) !!}">
+                        {{ $post['user_name'] }}
+                    </a>
+                </div>
+                <div>
+                    <i class="date">
+                        {{ $post['post_date'] }}
+                    </i> 
+                </div>
             </div>
-            <div>
-                <a class="user" href="{!! url('/post/user', [$post['user_id']]) !!}">
-                    {{ $post['user_name'] }}
-                </a>
+        @empty
+            <div class="card rounded">
+                <h2>No hay publicaciones</h2>
             </div>
-            <div>
-                <i class="date">
-                    {{ $post['post_date'] }}
-                </i> 
-            </div>
-        </div>
-    @empty
-        <div class="card rounded">
-            <h2>No hay publicaciones</h2>
-        </div>
-    @endforelse
+        @endforelse
+    </div>
 </body>
 </html>
