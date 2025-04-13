1b289e72bcf712bc180777b76c0b6678473f0935
Eduardo Mulhia <77212608+3l-the@users.noreply.github.com>
2025-04-12 20:21:37 -0600

    Video de Youtube agregado

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,33 +9,38 @@
     @include('common.header')
 </header>
 <body>
-    <div class="container mt-3">
-        <h3>Ultimas publicaciones</h3>
-    </div>
-    <div class="container flex-wrap mt-3">
-        @forelse($posts as $post)
-            <div class="card mb-2 rounded">
-                <div>
-                    <a class="subject" href="{!! url('/post/detail', [$post['post_id']]) !!}">
-                        {{ $post['post_subject'] }}
-                    </a> 
-                </div>
-                <div>
-                    <a class="user" href="{!! url('/post/user', [$post['user_id']]) !!}">
-                        {{ $post['user_name'] }}
-                    </a>
-                </div>
-                <div>
-                    <i class="date">
-                        {{ $post['post_date'] }}
-                    </i> 
-                </div>
+    <div class="container d-flex flex wrap">
+        <div class="container mt-3">
+            <h3>Ultimas publicaciones</h3>
+            <div class="container flex-wrap mt-3">
+                @forelse($posts as $post)
+                    <div class="card mb-2 rounded">
+                        <div>
+                            <a class="subject" href="{!! url('/post/detail', [$post['post_id']]) !!}">
+                                {{ $post['post_subject'] }}
+                            </a> 
+                        </div>
+                        <div>
+                            <a class="user" href="{!! url('/post/user', [$post['user_id']]) !!}">
+                                {{ $post['user_name'] }}
+                            </a>
+                        </div>
+                        <div>
+                            <i class="date">
+                                {{ $post['post_date'] }}
+                            </i> 
+                        </div>
+                    </div>
+                @empty
+                    <div class="card rounded">
+                        <h2>No hay publicaciones</h2>
+                    </div>
+                @endforelse
             </div>
-        @empty
-            <div class="card rounded">
-                <h2>No hay publicaciones</h2>
-            </div>
-        @endforelse
+        </div>
+        <div class="mt-3">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/3kA9kdk6Q18?si=IbviWHb0qmONEwgN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
     </div>
 </body>
 </html>
